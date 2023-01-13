0b19ebdc3a31ea4ed6d0f4b502601e3bd6c028c2
Flávio Caliman <flaviocaliman@hotmail.com>
2023-01-13 15:10:00 +0000

    Atualização do Costs
    Atualização de favicon e das páginas Company e Contact

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,8 +1,12 @@
+import styles from './Company.module.css'
+
 function Company() {
     return (
-        <div>
+        <div className={styles.company_container}>
             <h1>Company</h1>
+            <li>Site para inserir e gerenciar custos de projetos pessoais, criado com React (bibliioteca do JavaScript), para praticar os ensinamentos do professor Matheus Battisti - Hora de Codar! </li>
         </div>
+            
     )
 }
 
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,10 @@
+import styles from './Contact.module.css'
+
 function Contact() {
     return (
-        <div>
+        <div className={styles.contact_container}>
             <h1>Contact</h1>
+            <li>E-mail: flaviocaliman@hotmail.com</li>
         </div>
     )
 }
